ba405de9f079da9e40546ccd3d7c3b4d114aeb16
Mukhammad <muxammed.toktogulov@gmail.com>
2026-05-18 17:30:52 +0600

    first commt

--- src/store/products-query.js
+++ src/store/products-query.js
@@ -1,10 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
-import { $mainApi } from '../lib/api';
+import { $authApi, $mainApi } from '../lib/api';
 export const useProductQuery = () => {
 	const { data, isPending } = useQuery({
 		queryKey: ['products'],
 		queryFn: async () => {
-			const { data } = await $mainApi.get('/products');
+			const { data } = await $authApi.get('/products');
 			return data.data;
 		},
 	});
